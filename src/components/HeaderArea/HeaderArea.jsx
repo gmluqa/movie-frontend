52f5b0d829e40833322a68544806f4a6538a623b
gmluqa <gmluqa@protonmail.com>
2022-11-16 15:41:47 +0100

    Placeholder logo added inside HeaderArea

--- src/components/HeaderArea/HeaderArea.jsx
+++ src/components/HeaderArea/HeaderArea.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./HeaderArea.css";
 
 const HeaderArea = () => {
-  return <div className="headerArea">I'm the header!</div>;
+  return (
+    <div className="headerArea">
+      <img
+        className="headerLogo"
+        src="https://bendblockbuster.com/wp-content/uploads/2019/05/bb-ticket.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default HeaderArea;
