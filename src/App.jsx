4812742afbb80eaef888a5e85da21f96ec3f5d08
gmluqa <gmluqa@protonmail.com>
2022-11-16 21:01:27 +0100

    Made body take in routes, now going to design header + buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header/Header";
-import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
+import Body from "./components/Body/Body";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Body />
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./Body.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Home/Home";
 
 const Body = () => {
-  return <div className="classBody">I'm the body</div>;
+  return (
+    <div className="classBody">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<h1>register!!</h1>} />
+        <Route path="/login" element={<h1>login!!</h1>} />
+        <Route path="/user-area" element={<h1>user-area!!</h1>} />
+        <Route path="/admin-area" element={<h1>admin area!!</h1>} />
+        <Route path="*" element={<h1>404 page not found</h1>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Body;
