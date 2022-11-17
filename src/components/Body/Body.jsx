97c6920f4381697d0b5cd094cf62bade84be361c
gmluqa <gmluqa@protonmail.com>
2022-11-17 09:43:07 +0100

    Body routes are laid out better, div placeholders instead of h1 for now

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -8,11 +8,11 @@ const Body = () => {
     <div className="classBody">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<h1>register!!</h1>} />
-        <Route path="/login" element={<h1>login!!</h1>} />
-        <Route path="/user-area" element={<h1>user-area!!</h1>} />
-        <Route path="/admin-area" element={<h1>admin area!!</h1>} />
-        <Route path="*" element={<h1>404 page not found</h1>} />
+        <Route path="/register" element={<div>register!!</div>} />
+        <Route path="/login" element={<div>login!!</div>} />
+        <Route path="/user-area" element={<div>user-area!!</div>} />
+        <Route path="/admin-area" element={<div>admin area!!</div>} />
+        <Route path="*" element={<div>404 page not found</div>} />
       </Routes>
     </div>
   );
