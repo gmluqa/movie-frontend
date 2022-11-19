2a9681650002f72e87767da59d3b14634a94ebb3
gmluqa <gmluqa@protonmail.com>
2022-11-19 14:54:25 +0100

    feat: Home component established, has TODO pending

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Carousel } from "antd";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Welcome to home</div>;
+  const contentStyle = {
+    margin: 20,
+    height: "50vh",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
+
+  return (
+    <>
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>Contenido 1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Contenido 2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Contenido 3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Contenido 4</h3>
+        </div>
+      </Carousel>
+
+      <div className="contentParent">
+        <div>This is some content</div>
+      </div>
+      <div className="contentParent">
+        <div>This is more content</div>
+      </div>
+      <div className="contentParent">
+        <div>This is more content</div>
+      </div>
+      <div className="contentParent">
+        <div>
+          This is more content, TODO: fix horizontal scroll that appears when
+          content expands the parent element
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
