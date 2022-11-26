afda926fe506f1143c74c2b2d9f1ec4836fbc748
Manel <quemonmanel@gmail.com>
2022-11-26 15:25:30 +0100

    feat:login

--- src/app/store.js
+++ src/app/store.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../components/userSlice';
+
+
 
 export default configureStore({
     reducer: {
         user: userSlice
+        
     }
     
 });
-=======
-import { configureStore } from "@reduxjs/toolkit"
-import registerSlice from "../components/Register/registerSlice"
-
-export default configureStore({
-    reducer: {
-        register: registerSlice
-    }
-})
->>>>>>> main
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from 'react-redux';
-import store from '../src/app/store'
+
 
 
 //RDX IMPORTS
@@ -15,7 +14,9 @@ import store from "./app/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
