1f9bbe7455d3cd301d72d792d178c969bd6c7bcb
gmluqa <gmluqa@protonmail.com>
2022-11-26 11:37:53 +0100

    chore: removed redundant import

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
-import registerSlice from "../components/Register/registerSlice"
 
 export default configureStore({
     reducer: {
-        register: registerSlice
+
     }
 })
