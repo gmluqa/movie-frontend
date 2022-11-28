1bbb25f7acdb292016e10462561762613f7a793c
gmluqa <gmluqa@protonmail.com>
2022-11-28 09:21:49 +0100

    Article structure and service defined

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Input } from "antd";
+const { TextArea } = Input;
 
 const Articles = () => {
+  const [criteria, setCriteria] = useState("");
+
+  const onChange = e => {
+    setCriteria(e.target.value);
+  };
+
+  useEffect(() => {
+    if (criteria !== "") {
+      //Voy a aplicar mi proceso de debounce....
+
+      const bring = setTimeout(() => {
+        searchFilms(criteria)
+          .then(res => {
+            //Guardo en RDX
+            // dispatch(addSearch({ movies: res.data.results }));
+          })
+          .catch(error => console.log(error));
+      }, 350);
+
+      return () => clearTimeout(bring);
+    } else if (criteria === "") {
+      //Guardo en RDX pelis vacías...
+      //   dispatch(cleanSearch({ movies: [] }));
+    }
+  }, [criteria]);
+
   return (
     <>
-      <div></div>
+      <Input
+        placeholder="Search for media here..."
+        allowClear
+        onChange={onChange}
+      />
+      <div>{criteria}</div>
     </>
   );
 };
