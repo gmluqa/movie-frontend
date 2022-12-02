2cb8c09c16e64e15029f0c2bb8a88f409b58bfba
gmluqa <gmluqa@protonmail.com>
2022-12-02 17:20:04 +0100

    feat: the sql query of rentals is rendered correctly

--- src/components/Rentals/Rentals.jsx
+++ src/components/Rentals/Rentals.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { getOrders } from "../../services/getUserOrders.service.js";
+
+// Retrieves JWT
+// Renders all articles user has as cards
+
+const userInfo = JSON.parse(localStorage.getItem("UserInfo"));
+const jwt = JSON.parse(localStorage.getItem("JWT"));
+
+const orders = await getOrders(userInfo, jwt);
+
+// Just use a hook and connect it to the thing
 
 const Rentals = () => {
-  return (
-    <>
-      <div>ads</div>
-    </>
-  );
+  const [articles, setArticles] = useState({});
+
+  {
+    /* This ternary checks if the article statehook is empty, if it is, it fills it with the users rentals, else does nothing*/
+    Object.entries(articles).length === 0 ? setArticles(orders) : void 0;
+  }
+
+  return <>{articles?.data?.[0]?.Product_ID}</>;
 };
 
 export default Rentals;
