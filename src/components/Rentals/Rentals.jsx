f3020b64f6aaf0545e1f8792ca79c454fc2608c1
gmluqa <gmluqa@protonmail.com>
2022-12-03 11:01:11 +0100

    Rentals are succesfully rendered

--- src/components/Rentals/Rentals.jsx
+++ src/components/Rentals/Rentals.jsx
@@ -1,8 +1,10 @@
 import { Card } from "antd";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import { getOrders } from "../../services/getUserOrders.service.js";
 import { getArticleById } from "../../services/getArticleById.service";
+const { Meta } = Card;
+import "../Articles/Articles.css";
 // Retrieves JWT
 // Renders all articles user has as cards
 
@@ -11,29 +13,46 @@ const jwt = JSON.parse(localStorage.getItem("JWT"));
 
 const orders = await getOrders(userInfo, jwt);
 
-const orderData = await getArticleById(1);
-
-let articlesArray = [];
 const articlesArrayMapper = async () => {
+  let arr = [];
   for (let i = 0; i < orders.data.length; i++) {
     const articleData = await getArticleById(orders.data[i].Product_ID);
-    articlesArray.push(articleData.data);
+    arr.push(articleData.data);
   }
+  return arr;
 };
-articlesArrayMapper();
-console.log(articlesArray);
-
 // Just use a hook and connect it to the thing
 
 const Rentals = () => {
-  const [articles, setArticles] = useState({});
+  const [articles, setArticles] = useState([]);
 
-  {
-    /* This ternary checks if the article statehook is empty, if it is, it fills it with the users rentals, else does nothing*/
-    Object.entries(articles).length === 0 ? setArticles(orders) : void 0;
-  }
+  useEffect(() => {
+    // On mount, state hook 'articles' array is filled with all movies
+    const promise = articlesArrayMapper();
 
-  return <></>;
+    promise.then(data => {
+      setArticles(data);
+    });
+  }, []);
+
+  const articlesRenderer = articles.map((item, index) => {
+    console.log(index);
+    return (
+      <Card
+        key={index}
+        className="cardComponent"
+        hoverable
+        style={{
+          width: 240,
+        }}
+        cover={<img alt={item.Name} src={item.Poster} />}
+      >
+        <Meta title={item.Name} description={item.Genre} />
+      </Card>
+    );
+  });
+
+  return <div className="cardsWrapper">{articlesRenderer}</div>;
 };
 
 export default Rentals;
