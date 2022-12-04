2d2ae7b0cdfe2bbc77721a262fff78694e21adf0
gmluqa <gmluqa@protonmail.com>
2022-12-04 13:47:54 +0100

    chore: JWT slicing null value fixed

--- src/services/userMakeRental.service.js
+++ src/services/userMakeRental.service.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3000'
 
 let returnedMessage = ""
-// JWT is obtained from local storage, the whole string containts " at the begginging and end, both of these are sliced out
-let JWT = localStorage.getItem("JWT").slice(1, -1)
+// JWT is obtained from local storage, 
+let JWT = localStorage.getItem("JWT")
+
+// Does the JWT exist? if yes, the " " at beginging and end are sliced out, if not, continue as normal 
+JWT ? JWT.slice(1, -1) : void (0)
 
 export let userMakeRental = async (values) => {
 
