3d601990e63710912400c1484e574558d31abee4
Manel <quemonmanel@gmail.com>
2022-12-05 03:02:17 +0100

    feat:userArea

--- src/components/UserArea/UserArea.jsx
+++ src/components/UserArea/UserArea.jsx
@@ -4,10 +4,11 @@ const { Meta } = Card;
 import "./UserArea.css";
 import { Link } from "react-router-dom";
 
+
 const UserArea = () => {
   return (
     <div className="cardsParent">
-      <Link to="./rentals" style={{ textDecoration: "none" }}>
+      <Link to="./rentals" style={{ textDecoration: "none"}}>
         <Card
           hoverable
           onClick={() =>
@@ -16,8 +17,9 @@ const UserArea = () => {
           style={{ width: 240 }}
           cover={
             <img
+              style={{width:"17.3em", height: "20em"}}
               alt="My rentals"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              src="https://pm1.narvii.com/7481/46b8b1fd05e76c95385332ba3d41aa64f9b72576r1-900-1179v2_hq.jpg"
               height={"200vh"}
             />
           }
@@ -27,13 +29,15 @@ const UserArea = () => {
       </Link>
       <Link to="./articles" style={{ textDecoration: "none" }}>
         <Card
+          
           hoverable
           onClick={() => console.log("this button renders /articles view")}
           style={{ width: 240 }}
           cover={
             <img
+            style={{width:"17.3em", height: "20em"}}
               alt="Make a rental"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              src="https://pm1.narvii.com/7481/2da09b2730ab8a36a0d8ff835cd2418da76ddc29r1-900-1163v2_hq.jpg"
               height={"200vh"}
             />
           }
@@ -41,22 +45,7 @@ const UserArea = () => {
           <Meta title="Make a rental" description="" />
         </Card>
       </Link>
-      <Link to="./subscriptions" style={{ textDecoration: "none" }}>
-        <Card
-          hoverable
-          onClick={() => console.log("this button renders /subscriptions view")}
-          style={{ width: 240 }}
-          cover={
-            <img
-              alt="Choose a subscription"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              height={"200vh"}
-            />
-          }
-        >
-          <Meta title="Choose a subscription" description="" />
-        </Card>
-      </Link>
+     
     </div>
   );
 };
